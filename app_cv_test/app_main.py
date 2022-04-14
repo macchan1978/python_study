c219b72a00ed7e092264fde9cf05c7a05256f30a
matsu45 <matsu.45@gmail.com>
2022-04-14 16:30:39 +0900

    ファイル分割(common等)

--- app_cv_test/app_main.py
+++ app_cv_test/app_main.py
@@ -2,36 +2,9 @@ from __future__ import annotations
 import tkinter as tk
 from tkinter import ttk
 import cv2
-from PIL import Image, ImageTk
-from pyparsing import col
+from tests.pyramidTestWindow import PyramidTestWindow
+from common import *
 
-
-class Point2i:
-    def __init__(self, x: int = 0, y: int = 0):
-        self.x = x
-        self.y = y
-
-
-class Piont2f:
-    def __init__(self, x: float = 0, y: float = 0):
-        self.x = x
-        self.y = y
-
-
-class Size:
-    def __init__(self, width: int = 0, height: int = 0):
-        self.width = width
-        self.height = height
-
-
-def createImageForCanvas(src: cv2.Mat, canvas=None):
-    img = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    imgPil = Image.fromarray(img)
-    imgTk = ImageTk.PhotoImage(imgPil)
-    if(not canvas is None):
-        canvas.create_image(0, 0, image=imgTk, anchor='nw')
-    # MEMO : PhotoImageはGCされないようにfieldで保持する必要がある
-    return imgTk
 
 
 class ImageWindow:
@@ -57,80 +30,7 @@ class ImageWindow:
         pass
 
 
-def gridCanvas(canvas: tk.Canvas, column: int = 0, row: int = 0, columnspan: int = 1):
-    canvas.grid(column=column, row=row, columnspan=columnspan)
-    return canvas
 
-
-class PyramidTestWindow:
-    def __init__(self, parent: tk.Tk):
-        self.size = size = Size(600, 400)
-        self.pyrLevel = 6
-        self._windows = win = tk.Toplevel(parent)
-        win.title('Pyramid Test Window')
-
-        # 解像度変更UI
-        frameUpper = ttk.Frame(win)
-        frameUpper.pack(anchor='ne')
-
-        self.label = label = ttk.Label(frameUpper, text="Hello World!")
-        label.grid(column=0, row=0)
-
-        ttk.Button(frameUpper, text="inc",
-                   command=lambda: self.incPyr(None)
-                   ).grid(column=1, row=0)
-
-        ttk.Button(frameUpper, text="dec",
-                   command=lambda: self.decPyr(None)
-                   ).grid(column=2, row=0)
-
-        # 2枚の画像を表示するUI
-        frameLower = ttk.Frame(win)
-        frameLower.pack()
-        self.canvas = gridCanvas(tk.Canvas(
-            master=frameLower, width=size.width, height=size.height, bg="White"),
-            column=0, row=0)
-
-        self.canvas2 = gridCanvas(tk.Canvas(
-            master=frameLower, width=size.width, height=size.height, bg="White"),
-            column=1, row=0)
-
-        # TODO : 画像パスのユーティリティ化
-        #img = cv2.imread('app_cv_test/images/soccer.jpg')
-        img = cv2.imread('app_cv_test/images/cat_image.jpg')
-        # 適切な縮小比率を計算
-        yRatio = size.height/img.shape[0]
-        xRatio = size.height/img.shape[1]
-        ratio = min(1, yRatio, xRatio)
-        resizedImg = cv2.resize(src=img, dsize=[0, 0], fx=ratio, fy=ratio)
-
-        # cv2.imread('app_cv_test/images/cat_image.jpg')
-        self.image = resizedImg
-        self.updatePyrLevelStr()
-
-    def updatePyrLevelStr(self):
-        self.label['text'] = f'Pyramid level : {self.pyrLevel}'
-        if(self.pyrLevel < 0):
-            return
-        pyrImage = self.image.copy()
-        for i in range(self.pyrLevel):
-            pyrImage = cv2.pyrDown(pyrImage)
-        self.tkImage = createImageForCanvas(pyrImage, self.canvas)
-        for i in range(self.pyrLevel):
-            pyrImage = cv2.pyrUp(pyrImage)
-        self.tkImage2 = createImageForCanvas(pyrImage, self.canvas2)
-
-    def incPyr(self, event):
-        self.pyrLevel += 1
-        self.updatePyrLevelStr()
-        pass
-
-    def decPyr(self, event):
-        self.pyrLevel -= 1
-        self.updatePyrLevelStr()
-        pass
-
-    pass
 
 # TODO : なんだか公式ではttkが勧められている?
 # https://docs.python.org/3/library/tkinter.ttk.html#using-ttk
