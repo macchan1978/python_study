a3217a3d8469ee658fd4c0d6fc9d7766491c66ae
matsu45 <matsu.45@gmail.com>
2022-04-15 10:13:57 +0900

    ColorSpaceTest,色域をUIで指定可能に

--- app_cv_test/tests/colorSpaceTestWindow.py
+++ app_cv_test/tests/colorSpaceTestWindow.py
@@ -12,30 +12,34 @@ class ColorSpaceTestWindow:
         win = tk.Toplevel(parent)
         win.title('Color Space Test Window')
 
-        # ボタンUI部---
         buttonUi = fluent.pack(ttk.Frame(win), anchor='w')
+        self.createButtonUi(buttonUi)
+
+        imageUi = fluent.pack(ttk.Frame(win))
+        self.canvas = fluent.pack(
+            tk.Canvas(imageUi, width=300, height=200, bg='white'))
+
+    def createButtonUi(self, buttonUi):
         # MEMO : **演算子を使うことで複数箇所に設定するキーワード引数をDRY化できる。
         opts = {'side': 'left'}
         fluent.pack(ttk.Button(
             buttonUi,
             text='open', command=lambda: self.openFile()),
             **opts)
-        self.textBox = fluent.pack(ttk.Entry(
+        self.textBoxColor = fluent.pack(ttk.Entry(
             buttonUi,
             width=10),
             **opts)
+        self.textBoxColor.insert(tk.END, '120')
+        self.textBoxColorRange = fluent.pack(ttk.Entry(
+            buttonUi,
+            width=10),
+            **opts)
+        self.textBoxColorRange.insert(tk.END, '10')
         fluent.pack(ttk.Button(
             buttonUi,
             text='apply', command=lambda: self.apply()),
             **opts)
-
-        # 画像表示部---
-        imageUi = fluent.pack(ttk.Frame(win))
-        self.canvas = fluent.pack(
-            tk.Canvas(imageUi, width=300, height=200, bg='white'),
-            **opts)
-
-        pass
 
     def openFile(self):
         print('openfile')
@@ -44,29 +48,33 @@ class ColorSpaceTestWindow:
             initialdir='/Users/shingo/dev/python/python_study/app_cv_test/images')
         if result is None:
             return
-        filePath = result.name
-
-        frame = cv2.imread(filePath)
-
-        # Convert BGR to HSV
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # define range of blue color in HSV
-        lower_blue = np.array([110, 50, 50])
-        upper_blue = np.array([130, 255, 255])
-        # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-        # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(frame, frame, mask=mask)
-
-        self.canvas["width"]=frame.shape[1]
-        self.canvas["height"]=frame.shape[0]
-        self.canvasImage = fluent.setCanvasImage(res, self.canvas)
+        self.image = cv2.imread(result.name)
+        self.processImage()
 
         pass
 
+    def processImage(self):
+        image = self.image.copy()
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        color = int(self.textBoxColor.get())
+        colorRange = int(self.textBoxColorRange.get())
+
+        # define range of blue color in HSV
+        lower_blue = np.array([color-colorRange, 50, 50])
+        upper_blue = np.array([color+colorRange, 255, 255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(image, image, mask=mask)
+
+        self.canvas["width"] = image.shape[1]
+        self.canvas["height"] = image.shape[0]
+        self.canvasImage = fluent.setCanvasImage(res, self.canvas)
+
     def apply(self):
-        print('apply')
-        self.textBox
+        self.processImage()
         pass
 
     pass
